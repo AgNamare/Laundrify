4d1db7a32a690fc0dfc1a2e8a3f9f6e9694e2c30
MbakaBrian <brianmbakanjc@gmail.com>
2025-03-31 12:17:21 +0300

    Order updates

---
+++ server/controllers/order.controller.js
@@ -0,0 +1,27 @@
+import asyncHandler from "express-async-handler";
+import { createOrderService, getOrdersService, getOrderService, updateOrderService, deleteOrderService } from "../services/order.service.js";
+
+export const createOrderHandler = asyncHandler(async (req, res) => {
+  const order = await createOrderService(req.body);
+  res.status(201).json(order);
+});
+
+export const getOrdersHandler = asyncHandler(async (req, res) => {
+  const orders = await getOrdersService();
+  res.json(orders);
+});
+
+export const getOrderHandler = asyncHandler(async (req, res) => {
+  const order = await getOrderService(req.params.orderId);
+  res.json(order);
+});
+
+export const updateOrderHandler = asyncHandler(async (req, res) => {
+  const order = await updateOrderService(req.params.orderId, req.body);
+  res.json(order);
+});
+
+export const deleteOrderHandler = asyncHandler(async (req, res) => {
+  await deleteOrderService(req.params.orderId);
+  res.status(204).send();
+});
