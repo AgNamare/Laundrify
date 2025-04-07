6939ca645011d1ccfbf3c00ded4021f3674b55e2
Antony Omondi <124069102+antonio-chief@users.noreply.github.com>
2025-04-07 08:47:26 +0300

    add history page

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,17 +2,19 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import UserReducer from "./userSlice.js";
 import OrderReducer from "./orderSlice.js"
+import LaundryReducer from "./slices/laundrySlice.js";
 import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   user: UserReducer,
   order: OrderReducer,
+  laundry: LaundryReducer,
 });
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["user"],
+  whitelist: ["user", "laundry"],
   version: 1,
 };
 
