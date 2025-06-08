f3f57a38f79bd441c391baa7a0b84ebac582188d
Augustine Namare <agnamare@gmail.com>
2025-06-08 11:35:28 +0300

    Fix deployment errors

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,10 @@
 import axios from "axios";
 
-const BASEURL = "https://laundrify.onrender.com";
-
 export default axios.create({
-  baseURL: BASEURL,
   withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: BASEURL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
